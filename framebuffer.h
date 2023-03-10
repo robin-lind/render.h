ef85704bf41023b6ec88efb7e7a57327fcf42359
Robin Lind <larsrobinlind@gmail.com>
2023-03-10 09:44:03 +0100

    3

--- framebuffer.h
+++ framebuffer.h
@@ -4,6 +4,15 @@
 template<typename TColor>
 struct framebuffer
 {
+	int width, height;
 	std::vector<TColor> pixels;
 	framebuffer() = default;
+	framebuffer(int _width, int _height) : width(_width), height(_height)
+	{
+		pixels.resize(width * height);
+	}
+	TColor& pixel(int x, int y)
+	{
+		return pixels[x + y * width];
+	}
 };
